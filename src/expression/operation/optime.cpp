828e2765c848b81c067a4845cbd5bad9b270a379
Alexandre Halbach <alexandre.halbach@gmail.com>
2017-11-03 12:52:53 +0100

    Adding optime

---
+++ src/expression/operation/optime.cpp
@@ -0,0 +1,29 @@
+#include "optime.h"
+
+
+std::vector<std::vector<densematrix>> optime::interpolate(elementselector& elemselect, std::vector<double>& evaluationcoordinates, expression* meshdeform)
+{
+	densematrix output(elemselect.countinselection(), evaluationcoordinates.size()/3, universe::currenttimestep);
+    
+    // This can only be on the cos0 harmonic:
+	return {{},{output}};
+}
+
+densematrix optime::multiharmonicinterpolate(int numtimeevals, elementselector& elemselect, std::vector<double>& evaluationcoordinates, expression* meshdeform)
+{
+    std::cout << "Error in 'optime' object: time variable 't' is not supported (non-periodic)" << std::endl;
+    abort();
+}
+
+std::shared_ptr<operation> optime::copy(void)
+{
+    std::shared_ptr<optime> op(new optime);
+    *op = *this;
+    op->reuse = false;
+    return op;
+}
+
+void optime::print(void)
+{
+    std::cout << "t";
+}
---
+++ src/expression/operation/optime.h
@@ -0,0 +1,33 @@
+// sparselizard - Copyright (C) 2017-2018 A. Halbach and C. Geuzaine, University of Liege
+//
+// See the LICENSE.txt file for license information. Please report all
+// bugs and problems to <alexandre.halbach at ulg.ac.be>.
+
+// This object treats the time variable 't'.
+
+#ifndef OPTIME_H
+#define OPTIME_H
+
+#include "operation.h"
+
+class optime: public operation
+{
+
+	private:
+        
+        bool reuse = false;
+        
+	public:
+        
+        std::vector<std::vector<densematrix>> interpolate(elementselector& elemselect, std::vector<double>& evaluationcoordinates, expression* meshdeform);
+        densematrix multiharmonicinterpolate(int numtimeevals, elementselector& elemselect, std::vector<double>& evaluationcoordinates, expression* meshdeform);
+
+        std::shared_ptr<operation> copy(void);
+        
+        void reuseit(bool istobereused) { reuse = istobereused; };
+        
+        void print(void);
+
+};
+
+#endif
