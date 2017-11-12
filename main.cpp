468d2ba8bb28c6bd14e89bd69dc33463897f74cf
Alexandre Halbach <alexandre.halbach@gmail.com>
2017-11-12 09:06:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,30 +11,41 @@ using namespace mathop;
 
 void sparselizard(void)
 {	
+    // The domain regions as defined in 'cirlce.geo':
     int vol = 1, sur = 2, top = 3;
     
     // The mesh can be curved!
     mesh mymesh("circle.msh");
     
+    // Nodal shape functions with 3 components.
+    // Field u is the membrane deflection.
     field u("h1xyz");
 
+    // Use interpolation order 3 on 'vol', the whole domain:
     u.setorder(vol, 3);
     
+    // Clamp on surface 'sur' (i.e. 0 valued-Dirichlet conditions):
     u.setconstraint(sur);
   
+    // E is Young's modulus. nu is Poisson's ratio. 
     parameter E, nu;
     E|vol = 150e9; nu|vol = 0.3;
   
     formulation elasticity;
 
+    // The linear elasticity formulation is classical and thus predefined:
     elasticity += integral(vol, predefinedelasticity(u, E, nu));
+    // Add a volumic force in the -z direction:
     elasticity += integral(vol, array1x3(0,0,-10)*tf(u));
 
     elasticity.generate();
 
     vec solu = solve(elasticity.A(), elasticity.b());
 
+    // Transfer the data from the solution vector to the u field:
     u.getdata(vol, solu);
+    // Write the deflection on the top surface of the membrane.
+    // Write with an order 3 interpolation.
     u.write(top, "u.pos", 3);
     
 }
