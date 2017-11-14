436b48de33eb7871db517dfcce1450e47cce9cb7
Alexandre Halbach <alexandre.halbach@gmail.com>
2017-11-14 10:17:55 +0100

    Update install_external_libs.sh

--- install_external_libs.sh
+++ install_external_libs.sh
@@ -17,6 +17,47 @@ cd ~/SLlibs;
 
 
 
+########## COMPILING OPENBLAS :
+
+echo '__________________________________________';
+echo 'FETCHING THE LATEST OPENBLAS VERSION FROM GIT';
+git clone https://github.com/xianyi/OpenBLAS.git openblas;
+echo '__________________________________________';
+echo 'COMPILING OPENBLAS';
+cd openblas;
+make -j4;
+mkdir install;
+make PREFIX=$(pwd)/install install;
+cd ..;
+
+
+########## COMPILING FFTW :
+
+echo '__________________________________________';
+echo 'FETCHING FFTW';
+mkdir fftw;
+cd fftw;
+fftwdir=$(pwd);
+mkdir install;
+
+if [ "$(uname)" == "Linux" ]; then
+wget http://www.fftw.org/fftw-3.3.7.tar.gz;
+elif [ "$(uname)" == "Darwin"  ]; then
+curl http://www.fftw.org/fftw-3.3.7.tar.gz -o fftw-3.3.7.tar.gz;
+fi
+
+tar -xf *.tar.gz;
+cd fftw*;
+echo '__________________________________________';
+echo 'CONFIGURING FFTW';
+./configure --prefix $fftwdir/install;
+echo '__________________________________________';
+echo 'COMPILING FFTW';
+make -j4;
+make install;
+cd ../..;
+
+
 ########## COMPILING PETSC WITH OPTIONS --download-mumps --download-scalapack --download-mpich --with-debugging=0 :
 
 echo '__________________________________________';
