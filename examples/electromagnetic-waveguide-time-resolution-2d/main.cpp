17a8f09a1e44bf8ac14a1cf3f4d7556b8053aff5
Alexandre Halbach <alexandre.halbach@gmail.com>
2017-11-16 19:32:59 +0100

    Update main.cpp

--- examples/electromagnetic-waveguide-time-resolution-2d/main.cpp
+++ examples/electromagnetic-waveguide-time-resolution-2d/main.cpp
@@ -74,7 +74,7 @@ void sparselizard(void)
         // Transfer the data to the electric field:
         E.getdata(wholedomain, solvec[ts]);
         // Write with an order 2 interpolation and with the name of your choice:
-        E.write(wholedomain, "u"+std::to_string(ts+100)+".pos",2); 
+        E.write(wholedomain, "E"+std::to_string(ts+100)+".pos",2); 
     }
 }
 
