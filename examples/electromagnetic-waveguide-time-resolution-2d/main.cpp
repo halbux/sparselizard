4589d21796eee2359d09b81a4d81b3cddb9c0637
Alexandre Halbach <alexandre.halbach@gmail.com>
2017-11-16 14:34:15 +0100

    Update main.cpp

--- examples/electromagnetic-waveguide-time-resolution-2d/main.cpp
+++ examples/electromagnetic-waveguide-time-resolution-2d/main.cpp
@@ -42,41 +42,41 @@ void sparselizard(void)
     // This is the weak formulation for electromagnetic waves in time:
     maxwell += integral(wholedomain, -curl(dof(E))*curl(tf(E)) - 1/(c*c)*dtdt(dof(E))*tf(E));
     
-	
-	// Define the Maxwell object to time-solve formulation 'maxwell' 
-	// with initial all zero solution vectors 'vec(maxwell)'.
-	// The general system to solve with Newmark is M*dtdtx + C*dtx + K*x = b.
-	//
-	// The last argument is a vector 'isconstant' telling if the:
-	//
-	// - excitation vector b is constant in time for isconstant[0]
-	// - matrix K is constant in time for isconstant[1]
-	// - matrix C is constant in time for isconstant[2]
-	// - matrix M is constant in time for isconstant[3]
-	//
-	// You can set isconstant[0] to 'true' if in b only the constraints
-	// are time dependent but the other excitation sources are not. 
-	//
-	// Setting properly the 'isconstant' vector can give a dramatic speedup
-	// since it may avoid reassembling or allow reusing the LU factorisation.
-	//
-	newmark nm(maxwell, vec(maxwell), vec(maxwell), {true, true, true, true});
-	 
- 	// Run the Newmark time resolution from the time in the first argument
- 	// to the time in the third argument by timesteps given as second argument.
- 	// The last argument is optional (default is 1). When set to an integer 'n'
- 	// only one every n timesteps is added to the output vector.
-	std::vector<vec> solvec = nm.runlinear(0, 0.05*1.0/freq, 20*1.0/freq, 2);
-	 
-	// Now save all data in the 'solvec' vector which contains the solution at every nth timestep.
-	for (int ts = 0; ts < solvec.size(); ts++)
-	{
-		 std::cout << ts << " ";
-		 // Transfer the data to the electric field:
-		 E.getdata(wholedomain, solvec[ts]);
-		 // Write with an order 2 interpolation and with the name of your choice:
-		 E.write(wholedomain, "u"+std::to_string(ts+100)+".pos",2); 
-	}
+    // Define the Maxwell object to time-solve formulation 'maxwell' 
+    // with initial all zero solution vectors 'vec(maxwell)'.
+    // The general system to solve with Newmark is M*dtdtx + C*dtx + K*x = b.
+    //
+    // The last argument is a vector 'isconstant' telling if the:
+    //
+    // - excitation vector b is constant in time for isconstant[0]
+    // - matrix K is constant in time for isconstant[1]
+    // - matrix C is constant in time for isconstant[2]
+    // - matrix M is constant in time for isconstant[3]
+    //
+    // You can set isconstant[0] to 'true' if in b only the constraints
+    // are time dependent but the other excitation sources are not. 
+    //
+    // Setting properly the 'isconstant' vector can give a dramatic speedup
+    // since it may avoid reassembling or allow reusing the LU factorisation.
+    //
+    newmark nm(maxwell, vec(maxwell), vec(maxwell), {true, true, true, true});
+    
+    // Run the Newmark time resolution from the time in the first argument
+    // to the time in the third argument by timesteps given as second argument.
+    // The last argument is optional (default is 1). When set to an integer 'n'
+    // only one every n timesteps is added to the output vector.
+    std::vector<vec> solvec = nm.runlinear(0, 0.05*1.0/freq, 20*1.0/freq, 2);
+    
+    // Now save all data in the 'solvec' vector which contains the solution at every nth timestep.
+    for (int ts = 0; ts < solvec.size(); ts++)
+    {
+        std::cout << ts << " ";
+        // Transfer the data to the electric field:
+        E.getdata(wholedomain, solvec[ts]);
+        // Write with an order 2 interpolation and with the name of your choice:
+        E.write(wholedomain, "u"+std::to_string(ts+100)+".pos",2); 
+        
+    }
 }
 
 int main(void)
