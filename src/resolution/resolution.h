96fa46cb1b5142b5d1baf0fac47ff522190baaa0
Alexandre Halbach <alexandre.halbach@gmail.com>
2017-11-23 08:39:30 +0100

    One include for all resolution methods

---
+++ src/resolution/resolution.h
@@ -0,0 +1,19 @@
+// sparselizard - Copyright (C) 2017-2018 A. Halbach and C. Geuzaine, University of Liege
+//
+// See the LICENSE.txt file for license information. Please report all
+// bugs and problems to <alexandre.halbach at ulg.ac.be>.
+
+#ifndef RESOLUTION_H
+#define RESOLUTION_H
+
+#include "newmark.h"
+
+class resolution
+{
+    private:
+        
+    public:
+
+};
+
+#endif
