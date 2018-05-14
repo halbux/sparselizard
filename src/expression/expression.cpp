07caeff08cef718bedf3b678988d7bba090680a8
Alexandre Halbach <alexandre.halbach@gmail.com>
2018-05-14 20:27:59 +0200

    Update expression.cpp

--- src/expression/expression.cpp
+++ src/expression/expression.cpp
@@ -218,7 +218,7 @@ std::vector<double> expression::max(int physreg, expression* meshdeform, int ref
             for (int d = 0; d < compxval.countrows() * compxval.countcolumns(); d++)
             {
                 bool isinboundedregion = true;
-                if (xyzrange.size() == 0)
+                if (xyzrange.size() > 0)
                     isinboundedregion = xyzrange[0] < xvalptr[d] && xyzrange[1] > xvalptr[d] && xyzrange[2] < yvalptr[d] && xyzrange[3] > yvalptr[d] && xyzrange[4] < zvalptr[d] && xyzrange[5] > zvalptr[d];
                 
                 if ( isinboundedregion && (maxval.size() == 0 || maxval[0] < valuesptr[d]) )	
