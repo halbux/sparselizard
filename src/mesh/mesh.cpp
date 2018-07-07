fff53b7b74f814a44d381d4beeee8b6cb0c8abed
halbux <alexandre.halbach@gmail.com>
2018-07-07 18:24:23 +0200

    Check geo

--- src/mesh/mesh.cpp
+++ src/mesh/mesh.cpp
@@ -188,7 +188,7 @@ void mesh::load(std::vector<shape> inputshapes, int verbosity)
 	{
 		if (inputshapes[i].getphysicalregion() <= 0)
 		{
-			std::cout << "Error in 'mesh' object: physical region number was negative or zero for shape '" << inputshapes[i].getname() << "'" << std::endl;
+			std::cout << "Error in 'mesh' object: physical region number was undefined (negative or zero) for shape '" << inputshapes[i].getname() << "'" << std::endl;
 			abort();
 		}
 	}
