aa0d04412a8014a09a862dc39d1086c2010a5a2b
Alexandre Halbach <alexandre.halbach@gmail.com>
2018-07-14 21:13:46 +0200

    Update mathop.h

--- src/expression/operation/mathop.h
+++ src/expression/operation/mathop.h
@@ -24,7 +24,7 @@ class field;
 
 namespace mathop
 {
-	double getpi(void);
+    double getpi(void);
 
     // Perform operations (union, intersection, exclusion) on physical regions:
     int regionunion(const std::vector<int> physregs);
