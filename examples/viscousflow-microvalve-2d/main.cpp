3a0e132f64248df9372467a2036621f8cb430ec2
halbux <alexandre.halbach@gmail.com>
2018-08-16 21:37:09 +0200

    Add viscous flow example 2D

---
+++ examples/viscousflow-microvalve-2d/main.cpp
@@ -0,0 +1,127 @@
+// This code simulates the 2D flow of a viscous fluid (water) in a microvalve.
+// Reynold's number rho*v*L/mu is very low so that the inertia terms can be neglected.
+// Since the velocities involved are very low the fluid can be considered incompressible.
+//
+// This code can be validated with analytical solutions of a flow between two
+// infinite parallel plates with a thin gap. Only the geometry has to be adapted.
+
+
+#include "sparselizardbase.h"
+
+
+using namespace mathop;
+
+void sparselizard(void)
+{
+	// Define the physical regions that will be used:
+	int support = 1, fluid = 2, pillar = 3, membrane = 4, inlet = 7, outlet = 8;
+
+
+	///// Define the microvalve geometry and mesh.
+
+	// Define the x and y geometrical dimensions:
+	double y1 = 15e-6, y2 = 25e-6, y3 = 30e-6;
+	double x1 = -100e-6, x2 = -50e-6, x3 = -25e-6, x4 = 25e-6, x5 = 50e-6, x6 = 100e-6;
+
+	// Define the mesh finesse:
+	int n = 10;
+
+	shape q1("quadrangle", support, {x1,0,0, x2,0,0, x2,y1,0, x1,y1,0}, {n,n,n,n});
+	shape q2("quadrangle", support, {x5,0,0, x6,0,0, x6,y1,0, x5,y1,0}, {n,n,n,n});
+
+	shape q3("quadrangle", fluid, {x2,0,0, x3,0,0, x3,y1,0, x2,y1,0}, {n,n,n,n});
+	shape q4("quadrangle", fluid, {x4,0,0, x5,0,0, x5,y1,0, x4,y1,0}, {n,n,n,n});
+
+	shape q5("quadrangle", pillar, {x3,0,0, x4,0,0, x4,y1,0, x3,y1,0}, {n,n,n,n});
+
+	shape q6("quadrangle", support, {x1,y1,0, x2,y1,0, x2,y2,0, x1,y2,0}, {n,n,n,n});
+	shape q7("quadrangle", support, {x5,y1,0, x6,y1,0, x6,y2,0, x5,y2,0}, {n,n,n,n});
+
+	shape q8("quadrangle", fluid, {x2,y1,0, x3,y1,0, x3,y2,0, x2,y2,0}, {n,n,n,n});
+	shape q9("quadrangle", fluid, {x4,y1,0, x5,y1,0, x5,y2,0, x4,y2,0}, {n,n,n,n});
+	shape q10("quadrangle", fluid, {x3,y1,0, x4,y1,0, x4,y2,0, x3,y2,0}, {n,n,n,n});
+
+	shape q11("quadrangle", membrane, {x1,y2,0, x2,y2,0, x2,y3,0, x1,y3,0}, {n,n,n,n});
+	shape q12("quadrangle", membrane, {x5,y2,0, x6,y2,0, x6,y3,0, x5,y3,0}, {n,n,n,n});
+	shape q13("quadrangle", membrane, {x2,y2,0, x3,y2,0, x3,y3,0, x2,y3,0}, {n,n,n,n});
+	shape q14("quadrangle", membrane, {x4,y2,0, x5,y2,0, x5,y3,0, x4,y3,0}, {n,n,n,n});
+	shape q15("quadrangle", membrane, {x3,y2,0, x4,y2,0, x4,y3,0, x3,y3,0}, {n,n,n,n});
+
+	// Get the inlet and outlet lines:
+	shape l1 = q3.getsons()[0];
+	shape l2 = q4.getsons()[0];
+
+	l1.setphysicalregion(inlet);
+	l2.setphysicalregion(outlet);
+
+	// Provide to the mesh all shapes of interest:
+    mesh mymesh({q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,l1,l2});
+    
+	// Write the mesh for display:
+	mymesh.write("microvalve.msh");
+
+
+	///// Code to simulate the fluid flow:
+
+	// Define the fluid boundary as the intersection between the solid and the fluid regions:
+	int solid = regionunion({pillar,support,membrane});
+	int fluidboundary = regionintersection({fluid,solid});
+
+	// Dynamic viscosity of water [Pa.s]:
+	double mu = 8.9e-4;
+
+	// Field v is the flow velocity. It uses nodal shape functions "h1" with two components.
+	// Field p is the pressure.
+	field v("h1xy"), p("h1");
+
+	// Force the flow velocity to 0 at the solid interface:
+	v.setconstraint(fluidboundary);
+
+	// Set a pressure of 100 Pa at the valve inlet and 0 at the outlet:
+	p.setconstraint(inlet, 100);
+	p.setconstraint(outlet);
+
+	// Use an order 1 interpolation for p and 2 for v on the fluid region:
+	p.setorder(fluid, 1);
+	v.setorder(fluid, 2);
+
+	// Define the weak formulation of the flow problem.
+	// The strong form can be found at https://en.wikipedia.org/wiki/Stokes_flow
+	formulation viscousflow;
+
+	viscousflow += integral(fluid, - mu*grad(compx(dof(v)))*grad(compx(tf(v))) - mu*grad(compy(dof(v)))*grad(compy(tf(v))) - grad(dof(p))*tf(v));	
+	viscousflow += integral(fluid, div(dof(v))*tf(p));	
+	
+	// Generate the algebraic matrices:
+	viscousflow.generate();
+
+	// Solve Ax = b:
+	vec sol = solve(viscousflow.A(), viscousflow.b());
+
+	// Transfer the data from the solution vector to the p and v fields:
+	p.setdata(fluid, sol);
+	v.setdata(fluid, sol);    
+
+	// Write the p and v fields to file. Write v with an order 2 interpolation.
+	p.write(fluid, "p.pos");
+	v.write(fluid, "v.pos", 2);
+
+	// Output the flowrate for a unith width:
+	double flowrate = (normal(inlet)*v).integrate(inlet, 4);
+	std::cout << std::endl << "Flowrate for a unit width: " << flowrate << " m^3/s" << std::endl;
+
+    // Code validation line. Can be removed.
+    std::cout << (flowrate < 1.4845e-7 && flowrate > 1.4844e-7);
+}
+
+int main(void)
+{	
+    SlepcInitialize(0,{},0,0);
+
+    sparselizard();
+
+    SlepcFinalize();
+
+    return 0;
+}
+
