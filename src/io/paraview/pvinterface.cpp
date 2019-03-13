585f4b78ab81329455897aaca4191dc32300679c
halbux <alexandre.halbach@gmail.com>
2019-03-13 08:41:06 +0100

    Preparing for paraview support

---
+++ src/io/paraview/pvinterface.cpp
@@ -0,0 +1,4 @@
+#include "pvinterface.h"
+
+
+
---
+++ src/io/paraview/pvinterface.h
@@ -0,0 +1,18 @@
+// sparselizard - Copyright (C) 2017- A. Halbach
+//
+// See the LICENSE.txt file for license information. Please report all
+// bugs and problems to <alexandre.halbach at gmail.com>.
+
+
+#ifndef PVINTERFACE_H
+#define PVINTERFACE_H
+
+#include <string>
+#include "densematrix.h"
+
+namespace pvinterface
+{
+
+};
+
+#endif
