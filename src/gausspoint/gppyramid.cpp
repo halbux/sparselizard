9db416f2e752193623dd5682a5c9a1e35e5db18c
Alexandre Halbach <34739072+halbux@users.noreply.github.com>
2019-04-14 14:37:56 +0200

    Update gppyramid.cpp

--- src/gausspoint/gppyramid.cpp
+++ src/gausspoint/gppyramid.cpp
@@ -2,7 +2,7 @@
 
 void gppyramid::set(int integrationorder, std::vector<double>& coordinates, std::vector<double>& weights)
 {
-	std::cout << "Error 'in gppyramid' namespace: Gauss points have not been defined yet for pyramids" << std::endl;
+	std::cout << "Error in 'gppyramid' namespace: Gauss points have not been defined yet for pyramids" << std::endl;
 	abort();
 }
 
