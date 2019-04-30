a94d15d2e558caeb978ae610a561b23373a4d110
halbux <alexandre.halbach@gmail.com>
2019-04-30 15:33:38 +0200

    Commit number 1000 celebration! Add error message on setdata vector with other than set or add

--- src/expression/vectorfieldselect.cpp
+++ src/expression/vectorfieldselect.cpp
@@ -3,5 +3,11 @@
 
 void vectorfieldselect::setdata(int physreg, field myfield, std::string op)
 {
+    if (op != "set" && op != "add")
+    {
+        std::cout << "Error in 'vectorfieldselect' object: operation " << op << " is unknown in .setdata (use 'set' or 'add')" << std::endl;
+        abort();
+    }
+	
     (myfield.getpointer())->transferdata(physreg, vectorfieldselect(myrawvec, myrawfield), op);
 }
