ef3863505b5df378412b12b84646e86868154966
halbux <alexandre.halbach@gmail.com>
2019-05-12 11:59:59 +0200

    Add configuration option for .med and .exo mesh format support

--- install_external_libs/install1_petsc.sh
+++ install_external_libs/install1_petsc.sh
@@ -13,11 +13,23 @@ mkdir ~/SLlibs;
 cd ~/SLlibs;
 
 
-########## COMPILING PETSC :
+echo '';
+echo 'Support for additional mesh formats can be added by editing this file and set the appropriate petsc configuration options.';
+echo 'WARNING: This requires a petsc configuration that takes longer and might in some cases need to be tuned to your system.';
+echo '';
+echo -n 'Press enter to continue without editing... ';
+read enterkey;
+
+
+########## DOWNLOAD PETSC :
 
 echo '__________________________________________';
 echo 'FETCHING THE LATEST PETSC VERSION FROM GIT';
 git clone -b maint https://bitbucket.org/petsc/petsc petsc;
+
+
+########## CONFIGURE PETSC (SELECT THE APPROPRIATE CONFIGURATION OPTIONS BELOW) :
+
 echo '__________________________________________';
 echo 'CONFIGURING PETSC';
 cd petsc;
@@ -30,7 +42,21 @@ PETSC_DIR=$(pwd);
 PETSC_ARCH=arch-darwin-c-opt;
 fi
 
-./configure --with-mpi=0 --with-mumps-serial=1 --download-mumps=1 --download-openblas --with-debugging=0 COPTFLAGS='-O3' CXXOPTFLAGS='-O3' FOPTFLAGS='-O3';
+# The configuration below does not add support for additional mesh formats but does not require mpi.
+./configure --with-mpi=0 --with-mumps-serial=1 --download-mumps=1 --download-openblas --with-debugging=0 --with-scalar-type=real COPTFLAGS='-O3' CXXOPTFLAGS='-O3' FOPTFLAGS='-O3';
+
+# The configuration below adds support for .exo and .med mesh formats (mpi is needed and it is therefore added to the configuration options).
+# Support for cgns can be added by manually installing cgns then providing the cgns folder to petsc.
+#
+# ADDITIONAL STEPS TO PERFORM FOR THIS CONFIGURATION OF PETSC:
+# --> INSTALL CMAKE, AUTOTOOLS AND AUTOCONF BEFORE RUNNING THE CONFIGURE COMMAND BELOW  (on Ubuntu type: sudo apt-get install cmake autotools-dev autoconf)
+# --> IN THE MAKEFILE REMOVE '-I ~/SLlibs/petsc/include/petsc/mpiuni' (otherwise the wrong mpi.h header is selected during make) 
+#
+#./configure --download-mpich --download-mumps --download-scalapack --download-openblas --download-med --download-hdf5 --download-zlib --download-netcdf --download-pnetcdf --download-exodusii --with-scalar-type=real --with-debugging=0 COPTFLAGS='-O3' CXXOPTFLAGS='-O3' FOPTFLAGS='-O3';
+
+
+########## COMPILE PETSC :
+
 echo '__________________________________________';
 echo 'COMPILING PETSC';
 make $PETSC_DIR $PETSC_ARCH all;
