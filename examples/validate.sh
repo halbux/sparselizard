9552e4a178a06b965a958fe617712bb048d8348a
Alexandre Halbach <34739072+halbux@users.noreply.github.com>
2019-07-09 10:51:37 +0200

    Update validate.sh

--- examples/validate.sh
+++ examples/validate.sh
@@ -8,10 +8,10 @@ cd ..;
 # LOAD REQUIRED LIBRARIES:
 
 if [ "$(uname)" == "Linux" ]; then
-LD_LIBRARY_PATH="$HOME/SLlibs/petsc/arch-linux2-c-opt/lib":"$HOME/SLlibs/slepc/arch-linux2-c-opt/lib":"$HOME/SLlibs/openblas/install/lib":$LD_LIBRARY_PATH
+LD_LIBRARY_PATH="$HOME/SLlibs/petsc/arch-linux2-c-opt/lib":$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH    
 elif [ "$(uname)" == "Darwin"  ]; then
-DYLD_LIBRARY_PATH="$HOME/SLlibs/petsc/arch-darwin-c-opt/lib":"$HOME/SLlibs/slepc/arch-darwin-c-opt/lib":"$HOME/SLlibs/openblas/install/lib":$DYLD_LIBRARY_PATH
+DYLD_LIBRARY_PATH="$HOME/SLlibs/petsc/arch-darwin-c-opt/lib":$DYLD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH
 fi
 
