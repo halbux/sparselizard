f56712581e9c7808e2243966b88159be323e7cc8
halbach <halbux@localhost.localdomain>
2019-07-27 13:26:41 +0200

    Shape function types now correspond to a number

--- src/shapefunction/hierarchical/hierarchicalformfunction.cpp
+++ src/shapefunction/hierarchical/hierarchicalformfunction.cpp
@@ -1,1 +1,34 @@
 #include "hierarchicalformfunction.h"
+
+
+int hierarchicalformfunction::gettypenumber(std::string fftypename)
+{
+    int outtypenum = -1;
+    for (int i = 0; i < mytypenames.size(); i++)
+    {
+        if (mytypenames[i] == fftypename)
+        {
+            outtypenum = i;
+            break;
+        }
+    }
+    
+    if (outtypenum == -1)
+    {
+        std::cout << "Error in 'hierarchicalformfunction' object: unknown type name " << fftypename << std::endl;
+        abort();
+    }
+    
+    return outtypenum;
+}
+
+std::string hierarchicalformfunction::gettypename(int fftypenumber)
+{
+    if (fftypenumber >= mytypenames.size())
+    {
+        std::cout << "Error in 'hierarchicalformfunction' object: unknown type number " << fftypenumber << std::endl;
+        abort();
+    }
+
+    return mytypenames[fftypenumber];
+}
--- src/shapefunction/hierarchical/hierarchicalformfunction.h
+++ src/shapefunction/hierarchical/hierarchicalformfunction.h
@@ -14,14 +14,22 @@
 #include "legendre.h"
 #include "polynomial.h"
 #include <vector>
+#include <string>
 #include "hierarchicalformfunctioncontainer.h"
 
 class hierarchicalformfunction
 {
-	private:
-	
-	public:
-	
+    private:
+    
+        // List all form function type names (type numbers are defined by the name ordering):
+        std::vector<std::string> mytypenames = {"h1","hcurl","q6","h11","one"};
+
+    public:
+
+        // Translate a form function type number to a type name and vice versa:
+        int gettypenumber(std::string fftypename);
+        std::string gettypename(int fftypenumber);
+
         // Get the number of form functions of order <= 'order':
         virtual int count(int order) {};
         // Get the number of form functions of order <= 'order' that are associated to the num th
@@ -30,7 +38,7 @@ class hierarchicalformfunction
         // - face   in case dim = 2 
         // - volume in case dim = 3 
         virtual int count(int order, int dim, int num) {};
-        
+
         // Get the number of components in the form function.
         virtual int countcomponents(void) {};
 
@@ -41,7 +49,7 @@ class hierarchicalformfunction
         // be carried out without taking care of the element orientation.
         // This provides an assembly speedup. By default it is not used.
         virtual bool isorientationdependent(int order) { return true; };
-        
+
         // Return a vector whose index i is true if the ith form function
         // associated to highest dimension elements is of gradient type.
         virtual std::vector<bool> isgradienttype(int order) { return std::vector<bool>(0); };
