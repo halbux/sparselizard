c8629f233bb128e5f17188e212069bf0bde18e1d
Alexandre Halbach <34739072+halbux@users.noreply.github.com>
2019-08-09 13:53:02 +0200

    Update rawtriangle.cpp

--- src/geometry/rawtriangle.cpp
+++ src/geometry/rawtriangle.cpp
@@ -161,7 +161,7 @@ void rawtriangle::mesh(void)
 
         for (int j = 0; j < n-i; j++)
         {
-
+            // Treat the three corners:
             if (i == 0 && j == n-1)
             {
                 mycoords[3*currentnode + 0] = xcorner[2];
