136957fb35f20d938cc784a6903c62b1a0c01d2d
halbach <halbux@localhost.localdomain>
2019-08-09 09:21:52 +0200

    Correct newly uploaded triangle shape in case it is deformed

--- src/geometry/rawtriangle.cpp
+++ src/geometry/rawtriangle.cpp
@@ -116,122 +116,142 @@ std::shared_ptr<rawshape> rawtriangle::getpointer(void)
 
 void rawtriangle::mesh(void)
 {
-	// Get the node coordinates on the lines:
-	std::vector<std::vector<double>*> linescoords(3);
-	std::vector<int> nummeshpts(3);
+    // Get the node coordinates on the lines:
+    std::vector<std::vector<double>*> linescoords(3);
+    std::vector<int> nummeshpts(3);
 
-	for (int i = 0; i < 3; i++)
-	{
-		linescoords[i] = sons[i]->getcoords();
-		nummeshpts[i] = linescoords[i]->size()/3;
-	}
+    for (int i = 0; i < 3; i++)
+    {
+        linescoords[i] = sons[i]->getcoords();
+        nummeshpts[i] = linescoords[i]->size()/3;
+    }
 
-	// Give an error if the edges do not have the same number of mesh nodes:
-	if (nummeshpts[0] != nummeshpts[1] || nummeshpts[0] != nummeshpts[2])
-	{
-		std::cout << "Error in 'rawtriangle' object: the number of nodes on all edges should be equal" << std::endl;
-		abort(); 
-	}
+    // Give an error if the edges do not have the same number of mesh nodes:
+    if (nummeshpts[0] != nummeshpts[1] || nummeshpts[0] != nummeshpts[2])
+    {
+        std::cout << "Error in 'rawtriangle' object: the number of nodes on all edges should be equal" << std::endl;
+        abort(); 
+    }
 
-	int n = nummeshpts[0];
+    int n = nummeshpts[0];
 
-	// Preallocate the coords and elems containers:
-	mycoords.resize(3* 0.5*n*(n+1));
-	myelems[2].resize(3*(n-1));
-	myelems[3].resize(4* 0.5*(n-2)*(n-1));
+    // Preallocate the coords and elems containers:
+    mycoords.resize(3* 0.5*n*(n+1));
+    myelems[2].resize(3*(n-1));
+    myelems[3].resize(4* 0.5*(n-2)*(n-1));
 
-	// Get the coordinates of the corner nodes:
-	std::vector<double> xcorner = {linescoords[0]->at(0), linescoords[1]->at(0), linescoords[2]->at(0)};
-	std::vector<double> ycorner = {linescoords[0]->at(1), linescoords[1]->at(1), linescoords[2]->at(1)};
-	std::vector<double> zcorner = {linescoords[0]->at(2), linescoords[1]->at(2), linescoords[2]->at(2)};
+    // Get the coordinates of the corner nodes:
+    std::vector<double> xcorner = {linescoords[0]->at(0), linescoords[1]->at(0), linescoords[2]->at(0)};
+    std::vector<double> ycorner = {linescoords[0]->at(1), linescoords[1]->at(1), linescoords[2]->at(1)};
+    std::vector<double> zcorner = {linescoords[0]->at(2), linescoords[1]->at(2), linescoords[2]->at(2)};
 
 
-	// Loop on all layers in the ki direction:
-	int currentnode = 0;
-	for (int i = 0; i < n; i++)
-	{	
-		// Coordinates of the first and last nodes in the line linking 
-		// the current node in line 0 and its counterpart in line 1:
-		double x1 = linescoords[0]->at(3*i+0);
-		double xn = linescoords[1]->at(3*(n-1-i)+0);
-		double y1 = linescoords[0]->at(3*i+1);
-		double yn = linescoords[1]->at(3*(n-1-i)+1);
-		double z1 = linescoords[0]->at(3*i+2);
-		double zn = linescoords[1]->at(3*(n-1-i)+2);
+    // Loop on all layers in the ki direction:
+    int currentnode = 0;
+    for (int i = 0; i < n; i++)
+    {	
+        // Coordinates of the first and last nodes in the line linking 
+        // the current node in line 0 and its counterpart in line 1:
+        double x1 = linescoords[0]->at(3*i+0);
+        double xn = linescoords[1]->at(3*(n-1-i)+0);
+        double y1 = linescoords[0]->at(3*i+1);
+        double yn = linescoords[1]->at(3*(n-1-i)+1);
+        double z1 = linescoords[0]->at(3*i+2);
+        double zn = linescoords[1]->at(3*(n-1-i)+2);
 
-		for (int j = 0; j < n-i; j++)
-		{
+        for (int j = 0; j < n-i; j++)
+        {
 
-			if (i == 0 && j == n-1)
-			{
-				mycoords[3*currentnode + 0] = xcorner[2];
-				mycoords[3*currentnode + 1] = ycorner[2];
-				mycoords[3*currentnode + 2] = zcorner[2];
-				currentnode++;
-				continue;
-			}
-			if (i == n-1 && j == 0)
-			{
-				mycoords[3*currentnode + 0] = xcorner[1];
-				mycoords[3*currentnode + 1] = ycorner[1];
-				mycoords[3*currentnode + 2] = zcorner[1];
-				currentnode++;
-				continue;
-			}
-			if (i == 0 && j == 0)
-			{
-				mycoords[3*currentnode + 0] = xcorner[0];
-				mycoords[3*currentnode + 1] = ycorner[0];
-				mycoords[3*currentnode + 2] = zcorner[0];
-				currentnode++;
-				continue;
-			}
+            if (i == 0 && j == n-1)
+            {
+                mycoords[3*currentnode + 0] = xcorner[2];
+                mycoords[3*currentnode + 1] = ycorner[2];
+                mycoords[3*currentnode + 2] = zcorner[2];
+                currentnode++;
+                continue;
+            }
+            if (i == n-1 && j == 0)
+            {
+                mycoords[3*currentnode + 0] = xcorner[1];
+                mycoords[3*currentnode + 1] = ycorner[1];
+                mycoords[3*currentnode + 2] = zcorner[1];
+                currentnode++;
+                continue;
+            }
+            if (i == 0 && j == 0)
+            {
+                mycoords[3*currentnode + 0] = xcorner[0];
+                mycoords[3*currentnode + 1] = ycorner[0];
+                mycoords[3*currentnode + 2] = zcorner[0];
+                currentnode++;
+                continue;
+            }
 
-			double xh1 = linescoords[2]->at(3*(n-1-j)+0);
-			double xhn = linescoords[1]->at(3*j+0);
-			double yh1 = linescoords[2]->at(3*(n-1-j)+1);
-			double yhn = linescoords[1]->at(3*j+1);
-			double zh1 = linescoords[2]->at(3*(n-1-j)+2);
-			double zhn = linescoords[1]->at(3*j+2);
+            double xh1 = linescoords[2]->at(3*(n-1-j)+0);
+            double xhn = linescoords[1]->at(3*j+0);
+            double yh1 = linescoords[2]->at(3*(n-1-j)+1);
+            double yhn = linescoords[1]->at(3*j+1);
+            double zh1 = linescoords[2]->at(3*(n-1-j)+2);
+            double zhn = linescoords[1]->at(3*j+2);
+            
+            double xd1 = linescoords[0]->at(3*(i+j)+0);
+            double xdn = linescoords[2]->at(3*(n-1-(i+j))+0);
+            double yd1 = linescoords[0]->at(3*(i+j)+1);
+            double ydn = linescoords[2]->at(3*(n-1-(i+j))+1);
+            double zd1 = linescoords[0]->at(3*(i+j)+2);
+            double zdn = linescoords[2]->at(3*(n-1-(i+j))+2);
+            
+            
+            // Undeformed mesh:
+            double mu = (double)i/(n-1);
+            double lambda = (double)j/(n-1-i);
 
-			double mu = (double)i/(n-1-j);
-			double lambda = (double)j/(n-1-i);
+            // Coordinates of the current node in the undeformed triangle:
+            double xundef = (1-lambda)*((1-mu)*xcorner[0]+mu*xcorner[1]) + lambda*((1-mu)*xcorner[2]+mu*xcorner[1]);
+            double yundef = (1-lambda)*((1-mu)*ycorner[0]+mu*ycorner[1]) + lambda*((1-mu)*ycorner[2]+mu*ycorner[1]);
+            double zundef = (1-lambda)*((1-mu)*zcorner[0]+mu*zcorner[1]) + lambda*((1-mu)*zcorner[2]+mu*zcorner[1]);
 
-			mycoords[3*currentnode + 0] = 0.5*( (1-lambda)*x1 + lambda*xn + (1-mu)*xh1 + mu*xhn );
-			mycoords[3*currentnode + 1] = 0.5*( (1-lambda)*y1 + lambda*yn + (1-mu)*yh1 + mu*yhn );
-			mycoords[3*currentnode + 2] = 0.5*( (1-lambda)*z1 + lambda*zn + (1-mu)*zh1 + mu*zhn );
 
-			currentnode++;
-		}
-	}
+            mu = (double)i/(n-1-j);
+            lambda = (double)j/(n-1-i);
+            // On the diagonal between nodes 1 and 2:
+            double kappa = (double)(j)/(i+j);
 
-	// Add the triangle elements:
-	currentnode = 0;
-	for (int i = 0; i < n-1; i++)
-	{
-		currentnode += n-i;
+            mycoords[3*currentnode + 0] = 0.5*( (1-lambda)*x1 + lambda*xn + (1-mu)*xh1 + mu*xhn + (1-kappa)*xd1 + kappa*xdn - xundef );
+            mycoords[3*currentnode + 1] = 0.5*( (1-lambda)*y1 + lambda*yn + (1-mu)*yh1 + mu*yhn + (1-kappa)*yd1 + kappa*ydn - yundef );
+            mycoords[3*currentnode + 2] = 0.5*( (1-lambda)*z1 + lambda*zn + (1-mu)*zh1 + mu*zhn + (1-kappa)*zd1 + kappa*zdn - zundef );
 
-		myelems[2][3*i+0] = currentnode-1;
-		myelems[2][3*i+1] = currentnode-2;
-		myelems[2][3*i+2] = currentnode+n-(i+1)-1;
-	}
+            currentnode++;
+        }
+    }
 
-	// Add the quadrangle elements:
-	int currentelem = 0; currentnode = 0;
-	for (int i = 0; i < n-2; i++)
-	{
-		for (int j = 0; j < n-2-i; j++)
-		{
-			myelems[3][4*currentelem+0] = currentnode+j+1;
-			myelems[3][4*currentelem+1] = currentnode+j;
-			myelems[3][4*currentelem+2] = currentnode+n-i+j;
-			myelems[3][4*currentelem+3] = currentnode+n-i+j+1;
+    // Add the triangle elements:
+    currentnode = 0;
+    for (int i = 0; i < n-1; i++)
+    {
+        currentnode += n-i;
 
-			currentelem++;
-		}
+        myelems[2][3*i+0] = currentnode-1;
+        myelems[2][3*i+1] = currentnode-2;
+        myelems[2][3*i+2] = currentnode+n-(i+1)-1;
+    }
 
-		currentnode += n-i;
-	}
+    // Add the quadrangle elements:
+    int currentelem = 0; currentnode = 0;
+    for (int i = 0; i < n-2; i++)
+    {
+        for (int j = 0; j < n-2-i; j++)
+        {
+            myelems[3][4*currentelem+0] = currentnode+j+1;
+            myelems[3][4*currentelem+1] = currentnode+j;
+            myelems[3][4*currentelem+2] = currentnode+n-i+j;
+            myelems[3][4*currentelem+3] = currentnode+n-i+j+1;
+
+            currentelem++;
+        }
+
+        currentnode += n-i;
+    }
 }
 
 
