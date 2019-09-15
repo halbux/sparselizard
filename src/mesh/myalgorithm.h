a9047a8e5caf6f8d7ac6c17198e6839bb577d367
halbach <halbux@localhost.localdomain>
2019-09-15 20:47:44 +0200

    Cleanup interpolate

--- src/mesh/myalgorithm.h
+++ src/mesh/myalgorithm.h
@@ -54,7 +54,6 @@ namespace myalgorithm
     int getroot(std::vector<polynomial>& poly, std::vector<double>& rhs, std::vector<double>& initialguess, double boxsize = 3, double tol = 1e-12, int maxit = 20);
 
     // Attempt to get the above mentionned root with multiple initial guesses provided in format {ki1,eta1,phi1,ki2,eta2,phi2,...}.
-    // An error is thrown instead of returning the -1 value mentionned above when Newton convergence is not obtained for any initial guess.
     int getrootmultiguess(std::vector<polynomial>& poly, std::vector<double>& rhs, std::vector<double>& initialguesses, std::vector<double>& kietaphi, double boxsize = 3, double tol = 1e-12, int maxit = 20);
     
     // Split the 'tosplit' vector into 'blocklen' vectors of length tosplit.size()/blocklen.
