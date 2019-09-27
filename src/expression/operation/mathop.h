9f4750dcdcb97a52e500cfbab699f5e3021b82f8
halbach <halbux@localhost.localdomain>
2019-09-27 15:43:48 +0200

    second push on tensor rotation

--- src/expression/operation/mathop.h
+++ src/expression/operation/mathop.h
@@ -121,6 +121,12 @@ namespace mathop
 
     // Get the trace of a square matrix:
     expression trace(expression a);
+    
+    // Get the rotation matrix for a 3x3 tensor with (x,y,z) component ordering.
+    // Input angles are in degrees.
+    expression rotation(double alphax, double alphay, double alphaz);
+    // Get the rotation matrix for a 6x6 tensor in Voigt ordering:
+    expression voigtrotation(double alphax, double alphay, double alphaz);
 
     // Get the determinant of the physical to reference element variable change Jacobian:
     expression detjac(void);
