db39688820b1ffde894ffb88bd18b13cd95e3ca0
Alexandre Halbach <34739072+halbux@users.noreply.github.com>
2019-10-06 19:05:43 +0200

    Update main.cpp

--- examples/piezoelectricity-bilayer-3d/main.cpp
+++ examples/piezoelectricity-bilayer-3d/main.cpp
@@ -33,7 +33,7 @@ void sparselizard(void)
     u.setorder(wholedomain, 2);
     v.setorder(pztlayer, 1);
     
-    // Clamp on surface 'sur' (i.e. 0 valued-Dirichlet conditions):
+    // Clamp on surface 'clamp' (i.e. 0 valued-Dirichlet conditions):
     u.setconstraint(clamp);
     
     v.harmonic(2).setconstraint(electrode, 10);	
