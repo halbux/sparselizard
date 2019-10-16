46db0e38a053e2c11f8cdd5adeb12a3d447a18a8
halbach <halbux@localhost.localdomain>
2019-10-16 08:27:04 +0200

    Update refcoordgroup

--- src/mesh/referencecoordinategroup.cpp
+++ src/mesh/referencecoordinategroup.cpp
@@ -1,10 +1,8 @@
 #include "referencecoordinategroup.h"
-#include "myalgorithm.h"
 
 
 referencecoordinategroup::referencecoordinategroup(std::vector<double>& coords)
 {
-    mynumcoords = coords.size()/3;
     mycoordgroup = coordinategroup(coords);
 }
 
@@ -12,85 +10,213 @@ void referencecoordinategroup::evalat(std::vector<int> inputdisjregs)
 {
     mydisjregs = inputdisjregs;
     
-    myelems = std::vector<int>(mynumcoords,-1);
-    mykietaphis = std::vector<double>(3*mynumcoords,0.0);
+    int numcoords = mycoordgroup.countcoordinates();
+    
+    std::vector<int> elems(numcoords,-1);
+    std::vector<int> coordnums(numcoords);
+    std::iota(coordnums.begin(), coordnums.end(), 0);
+    std::vector<double> kietaphis(3*numcoords,0.0);
     
     for (int d = 0; d < mydisjregs.size(); d++)
-        myalgorithm::getreferencecoordinates(mycoordgroup, mydisjregs[d], myelems, mykietaphis);
-        
-    int myrangebegin = 0;
-}
-
-void referencecoordinategroup::group(void)
-{
-    // Sort first according to the element number then according to the reference coordinates:
-    std::vector<int> myreorderingvector;
-    myalgorithm::stablecoordinatesort({noisethreshold,noisethreshold,noisethreshold}, myelems, mykietaphis, myreorderingvector);
+        myalgorithm::getreferencecoordinates(mycoordgroup, mydisjregs[d], elems, kietaphis);    
     
-    std::vector<int> myelemscopy = myelems;
-    std::vector<double> mykietaphiscopy = mykietaphis;
-    for (int i = 0; i < mynumcoords; i++)
+    // Remove the negative elements (not found):
+    int numpositive = 0;
+    for (int i = 0; i < numcoords; i++)
     {
-        myelems[i] = myelemscopy[myreorderingvector[i]];
-        mykietaphis[3*i+0] = mykietaphiscopy[3*myreorderingvector[i]+0];
-        mykietaphis[3*i+1] = mykietaphiscopy[3*myreorderingvector[i]+1];
-        mykietaphis[3*i+2] = mykietaphiscopy[3*myreorderingvector[i]+2];
+        if (elems[i] >= 0)
+            numpositive++;
     }
-    myunorderingvector.resize(mynumcoords);
-    for (int i = 0; i < mynumcoords; i++)
-        myunorderingvector[myreorderingvector[i]] = i;
+    if (numpositive == 0)
+        return;
+
+    int curindex = 0;
+    for (int i = 0; i < numcoords; i++)
+    {   
+        if (elems[i] >= 0)
+        {
+            elems[curindex] = elems[i];
+            coordnums[curindex] = coordnums[i];
+            kietaphis[3*curindex+0] = kietaphis[3*i+0];
+            kietaphis[3*curindex+1] = kietaphis[3*i+1];
+            kietaphis[3*curindex+2] = kietaphis[3*i+2];
+            curindex++;
+        }
+    }
+    elems.resize(numpositive);
+    coordnums.resize(numpositive);
+    kietaphis.resize(3*numpositive);
     
-    // Move past the negative element numbers:
-    while (myelems[myrangebegin] < 0)
-        myrangebegin++;
+    // Sort according to the element numbers:
+    std::vector<int> reorderingvector;
+    myalgorithm::stablesort(elems, reorderingvector);
+    std::vector<int> elemscp = elems;
+    std::vector<int> coordnumscp = coordnums;
+    std::vector<double> kietaphiscp = kietaphis;
+    for (int i = 0; i < elems.size(); i++)
+    {
+        elems[i] = elemscp[reorderingvector[i]];
+        coordnums[i] = coordnumscp[reorderingvector[i]];
+        kietaphis[3*i+0] = kietaphiscp[3*reorderingvector[i]+0];
+        kietaphis[3*i+1] = kietaphiscp[3*reorderingvector[i]+1];
+        kietaphis[3*i+2] = kietaphiscp[3*reorderingvector[i]+2];
+    }
+
+    // Count the number of elements with a given number of ref coords:
+    int maxnumrefcoords = 1; int curnumrefcoords = 1;
+    for (int i = 1; i < elems.size(); i++)
+    {
+        if (elems[i] == elems[i-1])
+        {
+            curnumrefcoords++;
+            if (maxnumrefcoords < curnumrefcoords)
+                maxnumrefcoords = curnumrefcoords;
+        }
+        else
+            curnumrefcoords = 1;
+    }
+    std::vector<int> numelemswithnumrefcoords(maxnumrefcoords+1,0);
+    numelemswithnumrefcoords[1] = 1;
+    curnumrefcoords = 1;
+    for (int i = 1; i < elems.size(); i++)
+    {
+        if (elems[i] == elems[i-1])
+        {
+            numelemswithnumrefcoords[curnumrefcoords]--;
+            numelemswithnumrefcoords[curnumrefcoords+1]++;
+            curnumrefcoords++;
+        }
+        else
+        {
+            numelemswithnumrefcoords[1]++;
+            curnumrefcoords = 1;
+        }
+    }
+    
+    // Split the elements into number of reference coordinates:
+    myelems.resize(maxnumrefcoords+1);
+    mycoordnums.resize(maxnumrefcoords+1);
+    mykietaphis.resize(maxnumrefcoords+1);
+    for (int n = 1; n <= maxnumrefcoords; n++)
+    {
+        int numelems = numelemswithnumrefcoords[n];
+        myelems[n].resize(numelems*n);
+        mycoordnums[n].resize(numelems*n);
+        mykietaphis[n].resize(3*numelems*n);
+    }
+    std::vector<int> curindexes(maxnumrefcoords+1,0);
+    int first = 0;
+    for (int i = 1; i <= elems.size(); i++)
+    {
+        if (i < elems.size() && elems[i] == elems[i-1])
+            continue;
+        else
+        {
+            // Number of ref coords for the current element:
+            int n = i-first;
+    
+            for (int j = 0; j < n; j++)
+            {
+                myelems[n][curindexes[n]] = elems[first+i];
+                mycoordnums[n][curindexes[n]] = coordnums[first+i];
+                mykietaphis[n][3*curindexes[n]+0] = kietaphis[3*(first+i)+0];
+                mykietaphis[n][3*curindexes[n]+1] = kietaphis[3*(first+i)+1];
+                mykietaphis[n][3*curindexes[n]+2] = kietaphis[3*(first+i)+2];
+                
+                curindexes[n]++;
+            }
+            first += n;
+        }
+    }
+    
+    // Sort the containers according to the reference coordinates:
+    for (int n = 1; n <= maxnumrefcoords; n++)
+    {
+        // Sort the reference coordinates in each element:
+        std::vector<int> reordvec;
+        myalgorithm::stablecoordinatesort({noisethreshold,noisethreshold,noisethreshold}, myelems[n], mykietaphis[n], reordvec);
+
+        std::vector<int> myelemsreordered(myelems.size());
+        std::vector<int> mycoordnumsreordered(myelems.size());
+        std::vector<double> mykietaphisreordered(3*myelems.size());
+        for (int i = 0; i < reordvec.size(); i++)
+        {
+            myelemsreordered[i] = myelems[n][reordvec[i]];
+            mycoordnumsreordered[i] = mycoordnums[n][reordvec[i]];
+            mykietaphisreordered[3*i+0] = mykietaphis[n][3*reordvec[i]+0];
+            mykietaphisreordered[3*i+1] = mykietaphis[n][3*reordvec[i]+1];
+            mykietaphisreordered[3*i+2] = mykietaphis[n][3*reordvec[i]+2];
+        }
+        // Sort by blocks of n coordinates:
+        myalgorithm::stablesort(noisethreshold, mykietaphisreordered, reordvec, n);
+    
+        for (int i = 0; i < reordvec.size(); i++)
+        {
+            myelems[n][i] = myelemsreordered[reordvec[i]];
+            mycoordnums[n][i] = mycoordnumsreordered[reordvec[i]];
+            mykietaphis[n][3*i+0] = mykietaphisreordered[3*reordvec[i]+0];
+            mykietaphis[n][3*i+1] = mykietaphisreordered[3*reordvec[i]+1];
+            mykietaphis[n][3*i+2] = mykietaphisreordered[3*reordvec[i]+2];
+        }
+    }
+    
+    myrangebegin = 0; mynumrefcoords = 1;
 }
 
 bool referencecoordinategroup::next(void)
 {
-    if (myrangebegin >= mynumcoords)
+    if (mynumrefcoords >= myelems.size())
         return false;
         
+    std::vector<int>* curels = &(myelems[mynumrefcoords]);
+    std::vector<int>* curcoords = &(mycoordnums[mynumrefcoords]);
+    std::vector<double>* curkep = &(mykietaphis[mynumrefcoords]);
+        
     // Get all reference coordinates in the first element:
-    int index = myrangebegin+1;
-    while (index < mynumcoords && myelems[index] == myelems[index-1])
-        index++;
-    int numrefcoords = index-myrangebegin;
-    mycurrefcoords.resize(3*numrefcoords);
-    for (int i = 0; i < 3*numrefcoords; i++)
-        mycurrefcoords[i] = mykietaphis[myrangebegin+i];
-    int curelem = myelems[myrangebegin];
+    mycurrefcoords.resize(3*mynumrefcoords);
+    for (int i = 0; i < 3*mynumrefcoords; i++)
+        mycurrefcoords[i] = curkep->at(myrangebegin+i);
+    int curelem = curels->at(myrangebegin);
         
     // Find all consecutive elements with close enough reference coordinates:
-    int curelemindex = myrangebegin+numrefcoords; bool continueit = true;
-    while (curelemindex < mynumcoords-(numrefcoords-1) && continueit)
+    int curelemindex = myrangebegin+mynumrefcoords; bool continueit = true;
+    while (curelemindex < curels->size() && continueit)
     {
-        for (int i = 0; i < numrefcoords; i++)
+        for (int i = 0; i < mynumrefcoords; i++)
         {
             double kiref = mycurrefcoords[3*i+0]; double etaref = mycurrefcoords[3*i+1]; double phiref = mycurrefcoords[3*i+2];
-            double kicur = mykietaphis[3*(curelemindex+i)+0]; double etacur = mykietaphis[3*(curelemindex+i)+1]; double phicur = mykietaphis[3*(curelemindex+i)+2];
-            if (myelems[curelemindex+i] != curelem || std::abs(kiref-kicur) > noisethreshold || std::abs(etaref-etacur) > noisethreshold || std::abs(phiref-phicur) > noisethreshold)
+            double kicur = curkep->at(3*(curelemindex+i)+0); double etacur = curkep->at(3*(curelemindex+i)+1); double phicur = curkep->at(3*(curelemindex+i)+2);
+            if (curels->at(curelemindex) != curelem || std::abs(kiref-kicur) > noisethreshold || std::abs(etaref-etacur) > noisethreshold || std::abs(phiref-phicur) > noisethreshold)
             {
-                curelemindex -= numrefcoords;
+                curelemindex -= mynumrefcoords;
                 continueit = false;
                 break;
             }
         }
-        curelemindex += numrefcoords;
+        curelemindex += mynumrefcoords;
     }
     
     // Get the list of elements of same reference coordinates:
-    int numelemsinrange = (curelemindex-myrangebegin)/numrefcoords;
+    int numelemsinrange = (curelemindex-myrangebegin)/mynumrefcoords;
     mycurelems.resize(numelemsinrange);
     for (int i = 0; i < numelemsinrange; i++)
-        mycurelems[i] = myelems[myrangebegin+numrefcoords*i];
+        mycurelems[i] = curels->at(myrangebegin+mynumrefcoords*i);
     
     // Get the numbers of the reference coordinates:
-    mycurcoordnums.resize(numelemsinrange*numrefcoords);
+    mycurcoordnums.resize(numelemsinrange*mynumrefcoords);
     for (int i = 0; i < mycurcoordnums.size(); i++)
-        mycurcoordnums[i] = myunorderingvector[myrangebegin+i];
+        mycurcoordnums[i] = curcoords->at(myrangebegin+i);
     
 
+
     myrangebegin = curelemindex;
+    
+    if (myrangebegin == curels->size())
+    {
+        mynumrefcoords++;
+        myrangebegin = 0;
+    }
+    
     
     return true;
 }
