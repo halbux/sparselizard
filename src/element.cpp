caacbdc9f7f0aa42e918051fdf5202f41c5f8499
Alexandre Halbach <34739072+halbux@users.noreply.github.com>
2020-04-06 21:27:36 +0300

    Update element.cpp

--- src/element.cpp
+++ src/element.cpp
@@ -1179,8 +1179,6 @@ std::vector<double> element::calculatecoordinates(std::vector<double>& refcoords
 
 void element::fullsplit(std::vector<std::vector<double>>& cornerrefcoords, std::vector<double>& nodecoords)
 {
-    cornerrefcoords = std::vector<std::vector<double>>(8,std::vector<double>(0));
-
     int throughedgenum = -1;
     if (gettypenumber() == 4)
         throughedgenum = choosethroughedge(nodecoords);
