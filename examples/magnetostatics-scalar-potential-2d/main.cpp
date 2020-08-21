fa6151a9caad9357f27b4a8f4c6a2bdb34fe80c9
Alexandre Halbach <alexandre.halbach@gmail.com>
2020-08-21 11:21:26 +0300

    Elec/magnetic total force calc made simple

--- examples/magnetostatics-scalar-potential-2d/main.cpp
+++ examples/magnetostatics-scalar-potential-2d/main.cpp
@@ -12,119 +12,101 @@ using namespace mathop;
 
 void sparselizard(void)
 {	
-	// The domain regions as defined in 'halbacharray.geo':
-	int magnet1 = 1, magnet2 = 2, magnet3 = 3, magnet4 = 4, magnet5 = 5, steel = 6, air = 7, zeropotential = 8;
+    // The domain regions as defined in 'halbacharray.geo':
+    int magnet1 = 1, magnet2 = 2, magnet3 = 3, magnet4 = 4, magnet5 = 5, steel = 6, air = 7, zeropotential = 8;
 
-	// The mesh can be curved!
-	mesh mymesh("halbacharray.msh");
+    // The mesh can be curved!
+    mesh mymesh("halbacharray.msh");
 
-	// Define new physical regions for convenience:
-	int magnets = regionunion({magnet1, magnet2, magnet3, magnet4, magnet5});
-	int wholedomain = regionunion({magnets, steel, air});
+    // Define new physical regions for convenience:
+    int magnets = regionunion({magnet1, magnet2, magnet3, magnet4, magnet5});
+    int wholedomain = regionunion({magnets, steel, air});
 
-	// Nodal shape functions 'h1' for the magnetic scalar potential 'phi'.
-	field phi("h1");
+    // Nodal shape functions 'h1' for the magnetic scalar potential 'phi'.
+    field phi("h1");
 
-	// Use interpolation order 2 on the whole domain:
-	phi.setorder(wholedomain, 2);
+    // Use interpolation order 2 on the whole domain:
+    phi.setorder(wholedomain, 2);
 
-	// The magnetic scalar potential (just like the electric potential)
-	// needs to be fixed at least at one node to have a well-posed problem.
-	// Here it is forced to 0 at a permanent magnet corner point.
-	phi.setconstraint(zeropotential);
+    // The magnetic scalar potential (just like the electric potential)
+    // needs to be fixed at least at one node to have a well-posed problem.
+    // Here it is forced to 0 at a permanent magnet corner point.
+    phi.setconstraint(zeropotential);
 
-	// Vacuum magnetic permeability [H/m]: 
-	double mu0 = 4*getpi()*1e-7;
+    // Vacuum magnetic permeability [H/m]: 
+    double mu0 = 4*getpi()*1e-7;
 
-	// Define the permeability in all regions:
-	parameter mu;
+    // Define the permeability in all regions:
+    parameter mu;
 
-	mu|air = mu0;
-	mu|steel = 1000*mu0;
-	mu|magnets = mu0;
-
-
-	formulation magnetostatic;
-
-	// In the absence of current sources the static Maxwell equations give:
-	// 
-	// curl h = 0
-	// 
-	// One can thus define a magnetic scalar potential 'phi' such that h = -grad(phi).
-	//
-	// Considering also that div(b) = 0 we get
-	//
-	// div(mu*(-grad(phi))) = 0
-	//
-	// with b = mu * h.
-	//
-	// In the permanent magnet region b = mu0 * (h + m),
-	// i.e. the material is non-magnetic but it is pre-magnetized by the magnetization vector m [A/m].
-	// We thus get:
-	// div(mu0*(-grad(phi)) + mu0*m) = 0
-	// 
-	// A permanent magnet with a [0, 800e3] A/m magnetization is considered.
-
-	// The weak form corresponding to the above equations:
-	magnetostatic += integral(wholedomain, -grad(dof(phi)) * mu * grad(tf(phi)) );
-
-	// This is when all magnets are oriented in the y direction:
-	magnetostatic += integral(magnets, array2x1(0, 800e3) * mu * grad(tf(phi)) );
-	// This is in Halbach configuration (to maximise the magnetic field above the array):
-	//magnetostatic += integral(magnet1, array2x1(-800e3, 0) * mu * grad(tf(phi)) );
-	//magnetostatic += integral(magnet2, array2x1(0, -800e3) * mu * grad(tf(phi)) );
-	//magnetostatic += integral(magnet3, array2x1(800e3, 0) * mu * grad(tf(phi)) );
-	//magnetostatic += integral(magnet4, array2x1(0, 800e3) * mu * grad(tf(phi)) );
-	//magnetostatic += integral(magnet5, array2x1(-800e3, 0) * mu * grad(tf(phi)) );
+    mu|air = mu0;
+    mu|steel = 1000*mu0;
+    mu|magnets = mu0;
 
 
-	magnetostatic.generate();
+    formulation magnetostatic;
 
-	// Solve the algebraic system Ax = b:
-	vec sol = solve(magnetostatic.A(), magnetostatic.b());
+    // In the absence of current sources the static Maxwell equations give:
+    // 
+    // curl h = 0
+    // 
+    // One can thus define a magnetic scalar potential 'phi' such that h = -grad(phi).
+    //
+    // Considering also that div(b) = 0 we get
+    //
+    // div(mu*(-grad(phi))) = 0
+    //
+    // with b = mu * h.
+    //
+    // In the permanent magnet region b = mu0 * (h + m),
+    // i.e. the material is non-magnetic but it is pre-magnetized by the magnetization vector m [A/m].
+    // We thus get:
+    // div(mu0*(-grad(phi)) + mu0*m) = 0
+    // 
+    // A permanent magnet with a [0, 800e3] A/m magnetization is considered.
 
-	// Transfer the data from the solution vector to the phi field:
-	phi.setdata(wholedomain, sol);
+    // The weak form corresponding to the above equations:
+    magnetostatic += integral(wholedomain, -grad(dof(phi)) * mu * grad(tf(phi)) );
 
-	// Write the magnetic scalar potential and the magnetic field with an order 2 interpolation.
-	phi.write(wholedomain, "phi.pos", 2);
-	norm(-grad(phi)).write(wholedomain, "hnorm.pos", 2);
-	(-grad(phi)).write(wholedomain, "h.pos");
-		
-	// Evaluate the magnetic field 1.5cm above the center of the magnet array:
-	std::vector<double> magfieldnorm = norm(-grad(phi)).interpolate(wholedomain, {0,0.02,0});
-	std::cout << "Magnetic field 1.5cm above the array center: " << magfieldnorm[0] << " A/m" << std::endl;
-
-	// Write 20 magnetic field lines starting on the top side of the magnet array:
-	shape ln("line", -1, {-0.025,0.01,0, 0.025,0.01,0}, 20);
-	(-grad(phi)).streamline(regionunion({air, steel}), "magneticfieldline.pos", ln.getcoords(), 0.01/5);
+    // This is when all magnets are oriented in the y direction:
+    magnetostatic += integral(magnets, array2x1(0, 800e3) * mu * grad(tf(phi)) );
+    // This is in Halbach configuration (to maximise the magnetic field above the array):
+    //magnetostatic += integral(magnet1, array2x1(-800e3, 0) * mu * grad(tf(phi)) );
+    //magnetostatic += integral(magnet2, array2x1(0, -800e3) * mu * grad(tf(phi)) );
+    //magnetostatic += integral(magnet3, array2x1(800e3, 0) * mu * grad(tf(phi)) );
+    //magnetostatic += integral(magnet4, array2x1(0, 800e3) * mu * grad(tf(phi)) );
+    //magnetostatic += integral(magnet5, array2x1(-800e3, 0) * mu * grad(tf(phi)) );
 
 
+    magnetostatic.generate();
 
-	// The MAGNETOSTATIC FORCE acting on the steel disk is computed below.
-	
-	// This field will hold the x and y component of the magnetic forces:
-	field magforce("h1xy");
-	magforce.setorder(wholedomain, 1);
+    // Solve the algebraic system Ax = b:
+    vec sol = solve(magnetostatic.A(), magnetostatic.b());
 
-	// The magnetic force is projected on field 'magforce' on the steel region.
-	// This is done with a formulation of the type dof*tf - force calculation = 0.
-	formulation forceprojection;
+    // Transfer the data from the solution vector to the phi field:
+    phi.setdata(wholedomain, sol);
 
-	forceprojection += integral(steel, dof(magforce)*tf(magforce));
-	// The force term integral must be performed on all elements in the steel region AND in the ELEMENT LAYER AROUND.
-	// This is obtained by considering the elements on the whole domain 'wholedomain'. Since the force must only be
-	// calculated on the steel disk, the test functions are only selected on the steel (this is done with tf(..., steel)). 
-	forceprojection += integral(wholedomain, - predefinedmagnetostaticforce(tf(magforce, steel), -grad(phi), mu));
+    // Write the magnetic scalar potential and the magnetic field with an order 2 interpolation.
+    phi.write(wholedomain, "phi.pos", 2);
+    norm(-grad(phi)).write(wholedomain, "hnorm.pos", 2);
+    (-grad(phi)).write(wholedomain, "h.pos");
 
-	forceprojection.generate();
-	vec solf = solve(forceprojection.A(), forceprojection.b());
-	magforce.setdata(wholedomain, solf);
+    // Evaluate the magnetic field 1.5cm above the center of the magnet array:
+    std::vector<double> magfieldnorm = norm(-grad(phi)).interpolate(wholedomain, {0,0.02,0});
+    std::cout << "Magnetic field 1.5cm above the array center: " << magfieldnorm[0] << " A/m" << std::endl;
 
-	std::cout << "Force acting on the steel disk (fx, fy) = (" << compx(magforce).integrate(steel, 5) << ", " << compy(magforce).integrate(steel, 5) << ") N per unit depth" << std::endl;
+    // Write 20 magnetic field lines starting on the top side of the magnet array:
+    shape ln("line", -1, {-0.025,0.01,0, 0.025,0.01,0}, 20);
+    (-grad(phi)).streamline(regionunion({air, steel}), "magneticfieldline.pos", ln.getcoords(), 0.01/5);
 
-	// Code validation line. Can be removed.
-	std::cout << (magfieldnorm[0] < 64963.8 && magfieldnorm[0] > 64963.6);
+
+    // Calculate and print the total force on the steel disk:
+    std::vector<double> totalforce = gettotalforce(steel, -grad(phi), mu);
+    printtotalforce(steel, -grad(phi), mu);
+
+
+    // Code validation line. Can be removed.
+    std::cout << (magfieldnorm[0] < 64963.8 && magfieldnorm[0] > 64963.6 && totalforce[0] < -58.3914 && totalforce[0] > -58.3918);
 }
 
 int main(void)
