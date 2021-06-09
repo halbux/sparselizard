3ad61ac8afb8ffdb6d2437c7b89f1c81eb55109a
Alexandre Halbach <34739072+halbux@users.noreply.github.com>
2021-06-09 14:50:35 +0300

    Update expression.cpp

--- src/expression/expression.cpp
+++ src/expression/expression.cpp
@@ -2239,7 +2239,7 @@ void expression::extractport(std::vector<port>& ports, std::vector<int>& dtorder
     // Loop on all elementary sum terms in the formulation:
     for (int i = 0; i < sumterms.size(); i++)
     {
-        // If needed multiply the sum term by a constant 1 to get a product:
+        // If needed make a multiply out of it:
         if (sumterms[i]->isproduct() == false)
         {
             std::shared_ptr<opproduct> op(new opproduct);
