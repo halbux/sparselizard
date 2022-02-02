e77992a90a4b55a5e3d297cb14c3c1946c43bf37
halbux <alexandre.halbach@gmail.com>
2022-02-02 13:57:39 +0200

    add prdimonly arg to pr::getelemlist

--- src/mesh/physicalregion.cpp
+++ src/mesh/physicalregion.cpp
@@ -145,7 +145,7 @@ void physicalregion::removeduplicatedelements(void)
     }
 }
 
-std::vector<std::vector<int>>* physicalregion::getelementlist(void)
+std::vector<std::vector<int>>* physicalregion::getelementlist(bool prdimonly)
 {
     // Populate the element list if empty.
     
@@ -165,7 +165,7 @@ std::vector<std::vector<int>>* physicalregion::getelementlist(void)
         std::vector<int> sizes(8,0);
         for (int d = 0; d < includesdisjointregion.size(); d++)
         {
-            if (includesdisjointregion[d] && mydisjointregions->getelementdimension(d) == myelementdimension)
+            if (includesdisjointregion[d] && (not(prdimonly) || mydisjointregions->getelementdimension(d) == myelementdimension))
                 sizes[mydisjointregions->getelementtypenumber(d)] += mydisjointregions->countelements(d);
         }
         for (int i = 0; i < 8; i++)
@@ -175,7 +175,7 @@ std::vector<std::vector<int>>* physicalregion::getelementlist(void)
         std::vector<int> curindexes(8,0);
         for (int d = 0; d < includesdisjointregion.size(); d++)
         {
-            if (includesdisjointregion[d] && mydisjointregions->getelementdimension(d) == myelementdimension)
+            if (includesdisjointregion[d] && (not(prdimonly) || mydisjointregions->getelementdimension(d) == myelementdimension))
             {
                 int typenum = mydisjointregions->getelementtypenumber(d);
                 int numelems = mydisjointregions->countelements(d);
